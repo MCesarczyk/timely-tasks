a3f96e493a4f1426c8625341b7ccd0deb2549e1b
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-06-12 01:26:53 +0200

    test: add spec to Task module

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "fixture tasks are valid" do
+    assert tasks(:one).valid?
+    assert tasks(:two).valid?
+  end
+
+  test "title must be present" do
+    task = Task.new(content: "No title", completed: false)
+    assert_not task.valid?
+    assert_includes task.errors[:title], "can't be blank"
+  end
+
+  test "completed can be true or false" do
+    assert_includes [true, false], tasks(:one).completed
+    assert_includes [true, false], tasks(:two).completed
+  end
+
+  test "content can be blank" do
+    task = Task.new(title: "Test", content: "Lorem ipsum", completed: false)
+    assert task.valid?
+  end
 end
