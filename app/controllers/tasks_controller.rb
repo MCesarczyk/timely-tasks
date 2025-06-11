1b3d90feb8a0a598dfdcaa464eb4d597e55e96ac
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-06-12 01:15:59 +0200

    feat: create crud for tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,46 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    render json: @tasks, status: :ok
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    render json: @task, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Task not found' }, status: :not_found
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      render json: @task, status: :created
+    else
+      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      render json: @task, status: :ok
+    else
+      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Task not found' }, status: :not_found
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    head :no_content
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Task not found' }, status: :not_found
+  end
+
+  private
+  def task_params
+    params.expect(task: [ :title ])
+  end
+end
